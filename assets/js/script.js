70d146f218b2ac70bef328e10881e97c03b92a1f
Aryan Rajput <144768314+AryanRajput5772@users.noreply.github.com>
2026-04-22 17:22:42 +0530

    Styling changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -249,3 +249,12 @@ menu.addEventListener("show.bs.collapse", function () {
 menu.addEventListener("hide.bs.collapse", function () {
   toggler.innerHTML = '<span class="navbar-toggler-icon"></span>';
 });
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("nav-scroll");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
